9348fdcd39b4843ca44cede8f119a22e3035ed53
yangwubo <770957262@qq.com>
2019-06-11 16:42:44 +0800

    commit base webview app show oc and js call each other

---
+++ IOS_Component/BaseWebviewApp/BaseWebviewApp/Webview/WebviewController.h
@@ -0,0 +1,17 @@
+//
+//  WebviewController.h
+//  BaseWebviewApp
+//
+//  Created by hongbo ni on 2019/6/6.
+//  Copyright © 2019 hongbo ni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WebviewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
